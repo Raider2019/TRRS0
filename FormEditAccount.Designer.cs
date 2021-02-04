8384f7f4455c33673170108a4ffbe3fdbcb9852d
mrwhitestar78@gmail.com <mrwhitestar78@gmail.com>
2021-02-04 20:54:34 +0200

    Name Lab

--- FormEditAccount.Designer.cs
+++ FormEditAccount.Designer.cs
@@ -35,11 +35,11 @@ namespace TPKR01
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(67, 60);
+            this.label1.Location = new System.Drawing.Point(26, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(13, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "1";
             // 
             // FormEditAccount
             // 
